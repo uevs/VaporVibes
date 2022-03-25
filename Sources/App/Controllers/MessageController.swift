52a99234dc30f6a8e078c009438167a0d86982b3
uevs <36189306+uevs@users.noreply.github.com>
2022-03-25 10:51:10 +0100

    added functionality

--- Sources/App/Controllers/MessageController.swift
+++ Sources/App/Controllers/MessageController.swift
@@ -3,16 +3,31 @@ import Vapor
 
 struct MessageController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        let messages = routes.grouped("messages")
+        let messages = routes.grouped("vibes")
         messages.get(use: index)
         messages.post(use: create)
         messages.group(":messageID") { message in
             message.delete(use: delete)
+            message.get(use: retrieve)
+            message.put(use: update)
         }
     }
 
-    func index(req: Request) async throws -> [Message] {
-        try await Message.query(on: req.db).all()
+    func index(req: Request) async throws -> String {
+        guard let message = try await Message.query(on: req.db).all().randomElement() else {
+            return "No good vibes yet :("
+        }
+        
+        return "Your good vibe is \(message.message) from \(message.from)"
+
+
+    }
+    
+    func retrieve(req: Request) async throws -> Message {
+        guard let message = try await Message.find(req.parameters.get("messageID"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        return message
     }
 
     func create(req: Request) async throws -> Message {
@@ -26,6 +41,20 @@ struct MessageController: RouteCollection {
             throw Abort(.notFound)
         }
         try await message.delete(on: req.db)
+        return .ok
+    }
+    
+    func update(req: Request) async throws -> HTTPStatus {
+        let newMessage = try req.content.decode(Message.self)
+
+        guard let message = try await Message.find(req.parameters.get("messageID"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        message.message = newMessage.message
+        message.from = newMessage.from
+        try await message.update(on: req.db)
+        
         return .ok
     }
 }
